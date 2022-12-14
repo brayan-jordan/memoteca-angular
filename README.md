3016afa11cd7a6129b471631e2bc47f285ecb31d
Brayan Vinicius Jordan <brayan.11fera@gmail.com>
2022-12-14 15:07:27 -0300

    finalizado memoteca

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
-# memoteca-angular
-Projeto desenvolvido para aprender as funcionalidades básicas do Angular 14, o projeto conta com: Navegação entre telas e comunicação com a API, tudo isso usando as ferramentas essenciais do Angular 14 (sem foco em estilização).
+# Memoteca
 
-# start project (my node version 16.18.0)
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.3.
 
-## back-end (json-server)
+## Development server
 
-- cd server
-- npm install
-- npm start
+Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
 
-## front-end
+## Code scaffolding
 
-- npm install
-- npm start
+Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
 
-## front-end main screen's:
+## Build
 
-![home screen](https://github.com/brayan-jordan/memoteca-angular/blob/master/home.PNG)
+Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
 
-![create ad modal](https://github.com/brayan-jordan/memoteca-angular/blob/master/criar-pensamento.PNG)
+## Running unit tests
+
+Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
+
+## Running end-to-end tests
+
+Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
+
+## Further help
+
+To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
-import { RodapeComponent } from './components/rodape/rodape.component';
-import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
-import { FormsModule } from '@angular/forms';
-import { ListarPensamentosComponent } from './components/pensamentos/listar-pensamentos/listar-pensamentos.component';
-import { PensamentoComponent } from './components/pensamentos/pensamento/pensamento.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ExcluirPensamentoComponent } from './components/pensamentos/excluir-pensamento/excluir-pensamento.component';
-import { EditarPensamentoComponent } from './components/pensamentos/editar-pensamento/editar-pensamento.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
+import { PensamentoComponent } from './componentes/pensamentos/pensamento/pensamento.component';
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
+import { BotaoCarregarMaisComponent } from './componentes/pensamentos/listar-pensamento/botao-carregar-mais/botao-carregar-mais.component';
 
 @NgModule({
   declarations: [
@@ -19,13 +20,20 @@ import { EditarPensamentoComponent } from './components/pensamentos/editar-pensa
     CabecalhoComponent,
     RodapeComponent,
     CriarPensamentoComponent,
-    ListarPensamentosComponent,
+    ListarPensamentoComponent,
     PensamentoComponent,
     ExcluirPensamentoComponent,
     EditarPensamentoComponent,
+    BotaoCarregarMaisComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule
+  ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
